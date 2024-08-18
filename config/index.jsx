7bed3519bc2774fdfdab89ac198641eb42a72d94
Jude <silentcodear@gmail.com>
2024-08-18 15:30:14 +0100

    refactor and testnet only

--- config/index.jsx
+++ config/index.jsx
@@ -1,7 +1,7 @@
 import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
 
 import { cookieStorage, createStorage } from 'wagmi'
-import { arbitrum, avalanche, mainnet, sepolia, polygon } from 'wagmi/chains'
+import { arbitrum, assetChainTestnet, avalanche, mainnet, polygon, polygonMumbai, sepolia } from 'wagmi/chains'
 
 // Get projectId from https://cloud.walletconnect.com
 // export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
@@ -17,7 +17,7 @@ export const metadata = {
 }
 
 // Create wagmiConfig
-const chains = [mainnet, sepolia, arbitrum, avalanche, polygon];
+const chains = [assetChainTestnet, polygonMumbai, sepolia];
 export const config = defaultWagmiConfig({
     chains,
     projectId,
